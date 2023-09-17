b3534222802c4ad61c140e3123351f214a2060fe
ASHUTOSH SINGH KUSHWAH <110011610+SecretServicess@users.noreply.github.com>
2023-09-17 20:22:15 +0530

    Update Solution.cpp

--- 1207. Unique Number of Occurrences/Solution.cpp
+++ 1207. Unique Number of Occurrences/Solution.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
 
+        unordered_map<int, int> countMap; 
+        // Create a hash map to store value-count pairs
+        
+        // Count the occurrences of each value in arr
+        for (int num : arr) {
+            countMap[num]++; 
+        // Increment the count for the current value
+        }
+        
+        unordered_set<int> uniqueCounts; 
+        // Create a set to store unique counts
+        
+        // Check if all counts are unique
+        for (const auto& pair : countMap) { // Iterate through the hash map
+            int count = pair.second; // Get the count for the current value
+            
+            if (uniqueCounts.count(count) > 0) {
+                return false; // Duplicate count found, return false
+            }
+            
+            uniqueCounts.insert(count); // Add the count to the set
+        }
+        
+        return true; // All counts are unique, return true
+    }
+};
